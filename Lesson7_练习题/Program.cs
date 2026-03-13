c55824508374e326a4bc39f5a24c1d8825a499b4
917946560-jpg <917946560@qq.com>
2026-03-13 22:07:54 +0800

    2026-03-13 补push 3月12日第23-26集 等练习

---
+++ Lesson7_练习题/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("隐式转换练习题");
+
+#region 练习题一
+//什么情况下会出现数据类型的隐式转换，请举例说明
+//大范围存小范围
+//double->float一>整形（无符号、有符号）一>char
+//decimal一>整形（无符号、有符号）一>char
+//long->int -> short ->sbyte
+//ulong-> uint -> ushort -> byte
+//无符号 没法 隐式存储 有符号的
+//有符号的 可以 隐式存储 无符号（范围大小）
+
+int i=1;
+short s = 1;
+i = s;
+
+
+#endregion
+
+#region 练习题二
+//请将自己名字的每一个字符转换成数字并打印出来
+//char j = '加';
+//ASII 码
+int jia = '加';
+Console.WriteLine(jia);
+int lai = '濑';
+Console.WriteLine(lai);
+int you = '友';
+Console.WriteLine(you);
+int xiang = '香';
+Console.WriteLine(xiang);
+
+Console.WriteLine("加濑友香对应的数字编号："+ jia + lai + you + xiang);
+
+#endregion
---
+++ Lesson8_类型转换/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("类型转换——显示转换");
+
+//显示转换——>手动处理 强制转换
+
+#region 知识点一 括号强转
+//作用  一般情况下 将高精度的类型强制转换为低精度
+//语法：  变量类型 变量名 =（变量类型）变量；
+//注意：  精度问题 范围问题
+
+//相同大类的整形
+//有符号整形
+sbyte sb = 1;
+short s = 1;
+int i = 40000;
+long l = 1;
+
+//括号强转 可能会出现范围问题 造成的异常
+s = (short)i;
+Console.WriteLine(s);
+
+i = (int)l;
+sb = (sbyte)s;
+sb = (sbyte)i;
+sb = (sbyte)l;
+
+// 无符号整形
+byte b = 1;
+uint ui = 1;
+b = (byte)ui;
+
+//浮点之间（精度丢失）
+float f = 1.1f;
+double d = 1.1234567890123456789f;
+f = (float)d;
+Console.WriteLine(f);
+
+//无符号和有符号
+uint ui2 = 1;
+int i2 = -1;
+//在强转时  一定要注意范围 不然得到的结果 可能有异常
+ui2 = (uint)i2;
+Console.WriteLine(ui2);
+
+i2 = (int)ui2;
+
+
+//浮点和整形 浮点数强转成整形时 会直接抛弃掉小数点后面的小数（不会四舍五入）
+i2 = (int)1.64f;
+Console.WriteLine(i2);
+
+//char和数值类型
+i2 = 'A';
+
+char c =（char）i2;
+Console.WriteLine(c);
+//bool和string 是不能够通过 括号强转的
+
+//bool bo= true;
+//int i3 =(bool)bo;
+//string str= "123";
+//i3 =(int)str;
+
+
+#endregion
+
+#region 知识点二 Parse法
+
+#endregion
+
+#region 知识点三 Convert法
+
+#endregion
+
+#region 知识点四 其它类型转string
+
+#endregion
